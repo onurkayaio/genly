c8071b38234aebf2ddfa37be57185348c7f43a13
Onur Kaya <onur.kaya@metglobal.com>
2018-09-12 23:02:10 +0300

    added bootstrap.

--- src/containers/home.js
+++ src/containers/home.js
@@ -12,7 +12,7 @@ class Home extends Component {
   }
 
   render() {
-    return <div>home component</div>;
+    return <div className="container">home component.</div>;
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import registerServiceWorker from "./registerServiceWorker";
-
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import promiseMiddleware from "redux-promise";
 
 // reducer
 import reducers from "./reducers";
+
+// containers
+import App from "./App";
 
 const store = applyMiddleware(promiseMiddleware)(createStore);
 
