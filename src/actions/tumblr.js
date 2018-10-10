38c14306234f3b21c4902b7ecc6f681ad412adb9
Onur Kaya <onur.kaya@metglobal.com>
2018-10-10 23:32:43 +0300

    added method of get posts.

--- src/actions/tumblr.js
+++ src/actions/tumblr.js
@@ -1,20 +1,46 @@
+// actions.
+import { GET_USER_TUMBLR_POSTS } from "./../actions/index";
+
 const tumblr_base_url = "https://api.tumblr.com/v2";
 const consumer_public_key =
-  "NF3QHodm2PoqByjVp4oTOSlV7QwZ9qzeIgnYPsS18j0dtWxZ4c";
+  "NF3QHodm2PoqByjVp4oTOSlV7QwZ9qzeIgnYPsS18j0dtWxZ4c"; // need to hide it.
 
-export function getUserSpotifyProfile() {
-  // TODO: burayı yeniden gözden geçir.
-  if (requestParams.headers.Authorization === null) {
-    checkTokenExists();
-  }
+var limit = 20;
+var offset = 0;
+var posts = [];
 
-  const request = fetch(
-    `${tumblr_base_url}/blog/good.tumblr.com/info`,
-    requestParams
-  ).then(response => response.json());
+export function getUserBlogPosts(blogName) {
+  var request = getPosts(blogName);
 
   return {
-    type: GET_USER_SPOTIFY_PROFILE,
+    type: GET_USER_TUMBLR_POSTS,
     payload: request
   };
+}
+
+function getPosts(blogName) {
+  async function getPostsRequest(blogName) {
+    var response = await fetch(
+      `${tumblr_base_url}/blog/${blogName}/posts/audio?api_key=${consumer_public_key}&limit=${limit}&offset=${offset}`
+    );
+
+    var postsData = await response.json();
+
+    if (postsData['meta']['status'] === 200) {
+      
+    }
+    
+    posts = posts.concat(postsData["response"]["posts"]);
+
+    if (postsData["response"]["posts"].length === 20) {
+      offset = offset + limit;
+      await getPosts(blogName);
+    } else {
+      return posts;
+    }
+
+    return posts;
+  }
+
+  return getPostsRequest(blogName);
 }
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
 
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+
+// actions
+import { getUserBlogPosts } from "../../actions/tumblr";
+
 // css.
 import "./search.css";
 
@@ -7,30 +13,52 @@ class Search extends Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
+    this.onKeyPress = this.onKeyPress.bind(this);
   }
 
-  handleChange(event) {
-    console.log(event.target.value);
+  onKeyPress = e => {
+    if (e.which === 13) {
+      this.handleChange(e.target.value);
+    }
+  };
+
+  handleChange(value) {
+    this.props.getUserBlogPosts(value);
   }
 
   render() {
     return (
-      <div className="search-componenet">
-        <input
-          onChange={this.handleChange}
-          type="input"
-          name="name"
-          className="question"
-          id="nme"
-          required
-          autoComplete="off"
-        />
-        <label htmlFor="nme">
-          <span>What's the blog name?</span>
-        </label>
+      <div>
+        <div className="search-componenet">
+          <input
+            onKeyPress={this.onKeyPress}
+            type="input"
+            name="name"
+            className="question"
+            id="nme"
+            required
+            autoComplete="off"
+          />
+          <label htmlFor="nme">
+            <span>What's the blog name?</span>
+          </label>
+        </div>
       </div>
     );
   }
 }
 
-export default Search;
+function mapStateToProps(state) {
+  return {
+    tumblr: state.posts
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ getUserBlogPosts }, dispatch);
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Search);
