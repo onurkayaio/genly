e932f0f6ccd844675cd242bc7297a98bc024286d
Onur Kaya <onur.kaya@metglobal.com>
2018-10-21 15:53:18 +0300

    changed search as component.

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,25 +1,59 @@
-import React from "react";
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import connect from "react-redux/es/connect/connect";
 
 // css.
 import "./search.css";
 
-const Search = ({ onChange }) => {
-  return (
-    <div className="search-componenet">
-      <input
-        onKeyPress={onChange}
-        type="input"
-        name="name"
-        className="question"
-        id="nme"
-        required
-        autoComplete="off"
-      />
-      <label htmlFor="nme">
-        <span>What's the blog name?</span>
-      </label>
-    </div>
-  );
-};
+// actions.
+import { getUserBlogPosts } from "../../actions/tumblr";
 
-export default Search;
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    if (event.which === 13) {
+      this.props.getUserBlogPosts(event.currentTarget.value);
+    }
+  }
+
+  render() {
+    return (
+      <div className="search-componenet">
+        <input
+          onKeyPress={this.handleChange}
+          type="input"
+          name="name"
+          className="question"
+          id="nme"
+          required
+          autoComplete="off"
+        />
+        <label htmlFor="nme">
+          <span>What's the blog name?</span>
+        </label>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    tumblr: state.tumblr
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    { getUserBlogPosts },
+    dispatch
+  );
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Search);
--- src/containers/dashboard/dashboard.js
+++ src/containers/dashboard/dashboard.js
@@ -1,26 +1,16 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
-import { bindActionCreators } from "redux";
-import { postUserPlaylist } from "../../actions/spotify";
-import { getUserBlogPosts } from "../../actions/tumblr";
 import connect from "react-redux/es/connect/connect";
 
 // components.
 import Search from "../../components/search/search";
 import Header from "../../components/header/header";
-import Playlist from "../../components/playlist/playlist";
 import Toaster from "../../components/toaster/toaster";
 
 // helpers.
 import { getToken } from "./../../helpers";
 
 class Dashboard extends Component {
-  handleChange(event) {
-    if (event.which === 13) {
-      this.props.getUserBlogPosts(event.currentTarget.value);
-    }
-  }
-
   render() {
     let { tumblr } = this.props;
     return (
@@ -28,13 +18,7 @@ class Dashboard extends Component {
         {getToken() ? (
           <div>
             <Header />
-            {tumblr.tracks.length > 0 ? (
-              <Playlist tracks={tumblr} />
-            ) : (
-              <div>
-                <Search onChange={this.handleChange.bind(this)} />
-              </div>
-            )}
+            <Search />
           </div>
         ) : (
           <div>
@@ -62,14 +46,6 @@ function mapStateToProps(state) {
   };
 }
 
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators(
-    { getUserBlogPosts, postUserPlaylist },
-    dispatch
-  );
-}
-
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
 )(Dashboard);
