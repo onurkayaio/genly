f85e7d5ef008a49fde77acdbe63b90148cb45e92
onurkayaio <onurkaya244@gmail.com>
2019-05-25 14:26:40 +0300

    add share tags for social media

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import thunkMiddleware from "redux-thunk";
-import rootReducer from "../reducers";
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import rootReducer from '../reducers';
 
 export default function storeConfiguration(initialState = {}) {
   const middlewares = [thunkMiddleware];
